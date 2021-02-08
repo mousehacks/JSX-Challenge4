a79bc5a3617c0b2814b1e635f0ebc874deaec891
mousehacks <77026371+mousehacks@users.noreply.github.com>
2021-02-08 23:02:57 +0100

    Initial commit

---
+++ src/Components/Heading.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Heading() {
+  const customStyle = { color: "" };
+  let greeting;
+  const date = new Date();
+  const currentTime = date.getHours();
+
+  if (currentTime < 12) {
+    greeting = "Good Morning";
+    customStyle.color = "red";
+  } else if (currentTime < 18) {
+    greeting = "Good Afternoon";
+    customStyle.color = "green";
+  } else {
+    greeting = "Good Night";
+    customStyle.color = "blue";
+  }
+
+  return (
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
+  );
+}
+
+export default Heading;
